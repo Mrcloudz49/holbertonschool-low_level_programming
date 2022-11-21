60260d8e8c7574a507a3b595ab62eedabec20c04
Mrcloudz49 <shawneealex49@gmail.com>
2022-11-20 16:32:57 -0800

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_tables_create - creates a hash table
+ * hash_table_create - creates a hash table
  * @size: size of the array
  * Return: a pointer to the newly created hash table
  */
